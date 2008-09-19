f1790851877216fa9231c0aa10fb07c4a52dee8d
Nathan R. Yergler <nathan@cc-cto.(none)>
2008-09-18 22:10:57 -0700

    Added missing Resource fields to support existing aggregation code.

--- src/org/creativecommons/learn/oercloud/Resource.java
+++ src/org/creativecommons/learn/oercloud/Resource.java
@@ -18,4 +18,37 @@ public interface Resource {
 	@rdf("http://purl.org/dc/terms/subject")
 	public Set<String>getSubjects();
 	public void setSubjects(Set<String> subjects);
+
+	@rdf("http://purl.org/dc/terms/description")
+	public String getDescription();
+	public void setDescription(String description);
+
+	@rdf("http://purl.org/dc/terms/type")
+	public Set<String> getTypes();
+	public void setTypes(Set<String> types);
+	
+	@rdf("http://purl.org/dc/terms/format")
+	public Set<String> getFormats();
+	public void setFormats(Set<String> formats);
+	
+	@rdf("http://purl.org/dc/terms/contributor")
+	public Set<String> getContributors();
+	public void setContributors(Set<String> contributors);
+	
+	@rdf("http://purl.org/dc/terms/creator")
+	public Set<String> getCreators();
+	public void setCreators(Set<String> creators);
+	
+	@rdf("http://purl.org/dc/terms/educationLevel")
+	public Set<String> getEducationLevels();
+	public void setEducationLevels(Set<String> educationLevels);
+	
+	@rdf("http://www.w3.org/2000/01/rdf-schema#seeAlso")
+	public Set<OaiResource> getSeeAlso();
+	public void setSeeAlso(Set<OaiResource> seeAlso);
+	
+	@rdf("http://purl.org/dc/terms/language")
+	public Set<String> getLanguages();
+	public void setLanguages(Set<String> languages);
+		
 }
